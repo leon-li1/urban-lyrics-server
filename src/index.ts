fde13bf50e6673eb2ebedaa6f98b320f9420f3fa
Leon Li <leonli369@gmail.com>
2020-12-19 22:57:52 -0500

    refactored some logic

--- src/index.ts
+++ src/index.ts
@@ -24,13 +24,10 @@ function isValidScrapeRequest(req: any): req is ScrapeRequest {
 
 // the request needs to contain a youtube video (song) title
 app.post("/scrape", async function (req, res) {
-  if (!isValidScrapeRequest(req)) {
-    res.status(400).json({ error: "Invalid request" });
-    return;
-  }
-  const {
-    body: { title },
-  } = req;
+  if (!isValidScrapeRequest(req))
+    return res.status(400).json({ error: "Invalid request" });
+
+  const title = req.body.title;
   try {
     const lyrics = await scrapeLyrics(title);
     res.status(200).json(lyrics);
@@ -46,35 +43,31 @@ type LyricResult = {
   geniusUrl: string;
 };
 
+// 1. search google.com
+// 2. click on the first link
+// 3. grab the lyrics from the newly loaded page
 async function scrapeLyrics(title: string): Promise<LyricResult> {
-  // 1. search google.com
-  // 2. click on the first link
-  // 3. grab the lyrics from the newly loaded page
-
   const page = await globalBrowser.newPage();
+
   try {
     await page.setRequestInterception(true);
-    let skippedResources = blockedUrls;
     // listen for the request event
     page.on("request", (request) => {
-      if (
-        !["document", "script", "xhr", "other"].includes(request.resourceType())
-      ) {
-        request.abort();
-        return;
-      }
+      const isRequiredReq = ["document", "script", "xhr", "other"].includes(
+        request.resourceType()
+      );
+      if (!isRequiredReq) return request.abort();
+
       // mywebsite.com/page?q=google.com => mywebsite.com/page and mywebsite.com/page# => mywebsite.com/page
       const requestUrl: string = (request as any)._url
         .split("?")[0]
         .split("#")[0];
+
       // Example: requestUrl = mywebsite.com/page, resource = mywebsite.com
-      if (
-        skippedResources.some((resource) => requestUrl.indexOf(resource) !== -1)
-      ) {
-        request.abort();
-      } else {
-        request.continue();
-      }
+      if (blockedUrls.some((resource) => requestUrl.indexOf(resource) !== -1))
+        return request.abort();
+
+      request.continue();
     });
 
     // %20 = space, %26 = &
@@ -85,28 +78,29 @@ async function scrapeLyrics(title: string): Promise<LyricResult> {
     await page.goto(googleSearchURL);
 
     const firstLink = await page.waitForXPath(
-      '//*[@id="rso"]/div[1]/div/div[1]/a', // <-- concerning (test it thoroughly)
-      {
-        timeout: 10000,
-      }
-    ); // <a></a> <-- anchor tag
+      '//*[@id="rso"]/div[1]/div/div[1]/a',
+      { timeout: 10000 }
+    );
     const geniusUrl = await firstLink.evaluate(
       (a: HTMLAnchorElement) => a.href
     );
     await firstLink.click();
+
+    // evaluate runs the function on the element IN THE PUPPETEER BROWSER (not node.js)
     const lyrics = await (
       await page.waitForXPath("//*[contains(@class,'Lyrics__Root')]|//section")
     ).evaluate((p: any) => p.innerText);
+
     const songTitle = await (
       await page.waitForXPath("//h1[contains(@class, 'itle')]")
     ).evaluate((p: any) => p.innerText);
+
     const artist = await (
       await page.waitForXPath(
         "//a[contains(@href,'https://genius.com/artists/') and contains(@class, 'rtist')]"
       )
-    )
-      // runs the function on the element IN THE PUPPETEER BROWSER (not node.js)
-      .evaluate((p: any) => p.innerText);
+    ).evaluate((p: any) => p.innerText);
+
     return {
       lyrics,
       songTitle,
@@ -155,9 +149,6 @@ const blockedUrls = [
   "gumgum.com",
 ];
 
-// say we run this from this ip address: 333.333.3.3, then it will be accessible from http://333.333.3.3:8000
-// say we have a website domain on 333.333.3.3 called leonswebsite.com with port redirection 8000 -> 80
-// then it will be accessible from http://leonswebsite.com
 app.listen(process.env.PORT || 8000, async function () {
   console.log("Server started.");
   globalBrowser = await puppeteer.launch({
